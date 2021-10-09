9308bfb029ac5904318bc047099bb572bfd90ed9
ITZ-CYBERKING <91153738+ITZ-CYBERKING@users.noreply.github.com>
2021-10-09 13:43:17 +0530

    Update and rename .env.sample to string_session.py

---
+++ string_session.py
@@ -0,0 +1,68 @@
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+import random
+from colorama import Fore, Style, Back
+
+
+electro = """
+╔╗──╔═══╦═══╦═══╦═╗─╔╦═══╦═══╦═══╦╗──╔╗
+║║──║╔══╣╔═╗║╔══╣║╚╗║╠╗╔╗║╔═╗║╔═╗║╚╗╔╝║  
+║║──║╚══╣║─╚╣╚══╣╔╗╚╝║║║║║║─║║╚═╝╠╗╚╝╔╝  
+║║─╔╣╔══╣║╔═╣╔══╣║╚╗║║║║║║╚═╝║╔╗╔╝╚╗╔╝   
+║╚═╝║╚══╣╚╩═║╚══╣║─║║╠╝╚╝║╔═╗║║║╚╗─║║   
+╚═══╩═══╩═══╩═══╩╝─╚═╩═══╩╝─╚╩╝╚═╝─╚╝   
+"""
+logo = """
+╭━━━┳╮╱╱╭━━━┳━━━┳━━━━┳━━━┳━━━╮
+┃╭━━┫┃╱╱┃╭━━┫╭━╮┃╭╮╭╮┃╭━╮┃╭━╮┃
+┃╰━━┫┃╱╱┃╰━━┫┃╱╰┻╯┃┃╰┫╰━╯┃┃╱┃┃
+┃╭━━┫┃╱╭┫╭━━┫┃╱╭╮╱┃┃╱┃╭╮╭┫┃╱┃┃
+┃╰━━┫╰━╯┃╰━━┫╰━╯┃╱┃┃╱┃┃┃╰┫╰━╯┃
+╰━━━┻━━━┻━━━┻━━━╯╱╰╯╱╰╯╰━┻━━━╯
+╭╮╱╭┳━━━┳━━━┳━━━┳━━╮╭━━━┳━━━━╮
+┃┃╱┃┃╭━╮┃╭━━┫╭━╮┃╭╮┃┃╭━╮┃╭╮╭╮┃
+┃┃╱┃┃╰━━┫╰━━┫╰━╯┃╰╯╰┫┃╱┃┣╯┃┃╰╯
+┃┃╱┃┣━━╮┃╭━━┫╭╮╭┫╭━╮┃┃╱┃┃╱┃┃
+┃╰━╯┃╰━╯┃╰━━┫┃┃╰┫╰━╯┃╰━╯┃╱┃┃
+╰━━━┻━━━┻━━━┻╯╰━┻━━━┻━━━╯╱╰╯
+"""
+baap_bolte = """
+#Legendary ElectroBot          
+Made With Love By Team ELECTRO
+"""
+                                                                                                            
+print("")
+print(Style.BRIGHT + Fore.MAGENTA + electro)
+print(Style.RESET_ALL)
+print(Style.BRIGHT + Fore.BLUE + logo)
+print(Style.RESET_ALL)
+print(Style.BRIGHT + Fore.CYAN + Back.BLUE + baap_bolte)
+print(Style.RESET_ALL)
+print("""Welcome To ElectroBot String Generator By @itz_cyberking_xD""")
+print("""Kindly Enter Your Details To Continue ! """)
+
+API_KEY = input("API_KEY: ")
+API_HASH = input("API_HASH: ")
+
+while True:
+    try:
+        with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
+            print("String Sent To Your Saved Message, Store It To A Safe Place!! ")
+            print("")
+            session = client.session.save()
+            client.send_message(
+                "me",
+                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @ELECTROBOT_SUPPORT For Any Help !",
+            )
+
+            print(
+                "Thanks for Choosing ElectroBot Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
+            )
+    except:
+        print("")
+        print(
+            "Wrong phone number \n make sure its with correct country code. Example : +919811099999 ! Kindly Retry"
+        )
+        print("")
+        continue
+    break
