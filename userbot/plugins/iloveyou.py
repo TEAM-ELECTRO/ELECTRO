cd5aa6fffad09ba40113058ddad575736a8c6b60
ITZ-CYBERKING <91153738+ITZ-CYBERKING@users.noreply.github.com>
2021-10-13 03:53:39 +0530

    Create iloveyou.py

---
+++ userbot/plugins/iloveyou.py
@@ -0,0 +1,105 @@
+# PLUGIN MADE BY CYBERKING
+FOR ElectroBot
+# KEEP CREDITS ELSE GAY
+
+import random, re
+from ElectroBot.utils import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="iloveyou ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("""😘😘😘😘😘😘😘😘
+😘😘😘😘😘😘😘😘
+             😘😘😘
+             😘😘😘
+             😘😘😘
+             😘😘😘
+             😘😘😘
+             😘😘😘
+             😘😘😘
+             😘😘😘 
+             😘😘😘
+😘😘😘😘😘😘😘😘
+😘😘😘😘😘😘😘😘\n
+
+
+😘😘
+😘😘
+😘😘
+😘😘
+😘😘
+😘😘
+😘😘
+😘😘
+😘😘😘😘😘😘😘😘
+😘😘😘😘😘😘😘😘\n
+⁭
+           😘😘😘😘😘
+      😘😘😘😘😘😘😘
+   😘😘                       😘😘
+ 😘😘                          😘😘
+😘😘                            😘😘
+😘😘                            😘😘
+ 😘😘                           😘😘
+   😘😘                       😘😘
+       😘😘😘😘😘😘😘
+            😘😘😘😘😘\n
+⁭
+😘😘                              😘😘
+  😘😘                          😘😘
+    😘😘                      😘😘
+      😘😘                  😘😘
+         😘😘             😘😘
+           😘😘         😘😘
+             😘😘     😘😘
+               😘😘 😘😘
+                  😘😘😘
+                       😘\n
+⁭
+😘😘😘😘😘😘😘😘
+😘😘😘😘😘😘😘😘
+😘😘
+😘😘
+😘😘😘😘😘😘
+😘😘😘😘😘😘
+😘😘
+😘😘
+😘😘😘😘😘😘😘😘
+😘😘😘😘😘😘😘😘\n
+
+😘😘                         😘😘
+  😘😘                    😘😘
+     😘😘              😘😘
+        😘😘        😘😘
+           😘😘  😘😘
+              😘😘😘
+                😘😘
+                😘😘
+                😘😘
+                😘😘
+                😘😘\n
+⁭
+        😘😘😘😘😘😘
+     😘😘😘😘😘😘😘
+   😘😘                     😘😘
+ 😘😘                         😘😘
+😘😘                           😘😘
+😘😘                           😘😘
+ 😘😘                         😘😘
+   😘😘                     😘😘
+      😘😘😘😘😘😘😘
+            😘😘😘😘😘\n
+⁭
+😘😘                      😘😘
+😘😘                      😘😘
+😘😘                      😘😘
+😘😘                      😘😘
+😘😘                      😘😘
+😘😘                      😘😘
+😘😘                      😘😘
+  😘😘                  😘😘
+      😘😘😘😘😘😘
+            😘😘😘😘""")
+      
